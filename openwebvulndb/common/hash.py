5eb1c3c05b120d606f501cc6b4aa8c32c96389c5
Louis-Philippe Huberdeau <lp@huberdeau.info>
2016-08-10 10:17:18 -0400

    Lookup for the version number in chunks

--- openwebvulndb/common/hash.py
+++ openwebvulndb/common/hash.py
@@ -7,31 +7,49 @@ from .models import Signature
 
 class HashCollector:
 
-    def __init__(self, *, path, hasher, prefix=""):
+    def __init__(self, *, path, hasher, prefix="", lookup_version=None):
         self.path = path
         self.hasher = hasher
         self.prefix = prefix
+        self.version_checker = VersionChecker(lookup_version)
 
     def collect(self):
         for path, dirs, files in walk(self.path):
             files = [f for f in files if f[-4:] != ".php"]
 
             for file in files:
+                self.version_checker.reset()
+
                 full_path = join(path, file)
                 relative = full_path[len(self.path):].strip("/")
-                hash = self.hasher.hash(full_path)
+                hash = self.hasher.hash(full_path, chunk_cb=self.version_checker)
 
-                yield Signature(path=join(self.prefix, relative), hash=hash, algo=self.hasher.algo)
+                yield Signature(path=join(self.prefix, relative), hash=hash, algo=self.hasher.algo,
+                                contains_version=self.version_checker.contains_version)
 
 
 class Hasher:
     def __init__(self, algo):
         self.algo = algo
 
-    def hash(self, file_path):
+    def hash(self, file_path, chunk_cb=lambda c: None):
         hash = hashlib.new(self.algo)
         with open(file_path, "rb") as fp:
             for chunk in iter(lambda: fp.read(4096), b""):
                 hash.update(chunk)
+                chunk_cb(chunk)
 
             return hash.hexdigest()
+
+
+class VersionChecker:
+    def __init__(self, version):
+        self.version = version.encode('utf-8') if version is not None else version
+        self.reset()
+
+    def reset(self):
+        self.contains_version = None
+
+    def __call__(self, chunk):
+        if self.version is not None and self.version in chunk:
+            self.contains_version = True
--- tests/common_test/hash_collector_test.py
+++ tests/common_test/hash_collector_test.py
@@ -1,7 +1,7 @@
 from unittest import TestCase
 from unittest.mock import MagicMock, call, patch, mock_open
 
-from openwebvulndb.common.hash import HashCollector, Hasher
+from openwebvulndb.common.hash import HashCollector, Hasher, VersionChecker
 from openwebvulndb.common.models import Signature
 
 
@@ -14,7 +14,8 @@ class HashCollectorTest(TestCase):
                 ("/some/path/random1234/js", [], ["index.js"]),
                 ("/some/path/random1234/css", [], ["custom.css"]),
             ]
-            collector = HashCollector(path="/some/path/random1234", hasher=MagicMock(), prefix="wp-content/plugins/my-plugin")
+            collector = HashCollector(path="/some/path/random1234", hasher=MagicMock(),
+                                      prefix="wp-content/plugins/my-plugin")
             collector.hasher.algo = "CONST"
             collector.hasher.hash.return_value = "12345"
 
@@ -23,14 +24,16 @@ class HashCollectorTest(TestCase):
             walk.assert_called_with("/some/path/random1234")
 
             collector.hasher.hash.assert_has_calls([
-                call("/some/path/random1234/readme.txt"),
-                call("/some/path/random1234/license.txt"),
-                call("/some/path/random1234/css/custom.css"),
-                call("/some/path/random1234/js/index.js"),
+                call("/some/path/random1234/readme.txt", chunk_cb=collector.version_checker),
+                call("/some/path/random1234/license.txt", chunk_cb=collector.version_checker),
+                call("/some/path/random1234/css/custom.css", chunk_cb=collector.version_checker),
+                call("/some/path/random1234/js/index.js", chunk_cb=collector.version_checker),
             ], any_order=True)
 
-            self.assertIn(Signature(path="wp-content/plugins/my-plugin/readme.txt", hash="12345", algo="CONST"), signatures)
-            self.assertIn(Signature(path="wp-content/plugins/my-plugin/css/custom.css", hash="12345", algo="CONST"), signatures)
+            self.assertIn(Signature(path="wp-content/plugins/my-plugin/readme.txt", hash="12345", algo="CONST"),
+                          signatures)
+            self.assertIn(Signature(path="wp-content/plugins/my-plugin/css/custom.css", hash="12345", algo="CONST"),
+                          signatures)
 
     def test_exclude_php_files(self):
         with patch('openwebvulndb.common.hash.walk') as walk:
@@ -39,7 +42,8 @@ class HashCollectorTest(TestCase):
                 ("/some/path/random1234/js", [], ["index.js", "index.php"]),
                 ("/some/path/random1234/css", [], ["custom.css", "index.php"]),
             ]
-            collector = HashCollector(path="/some/path/random1234", hasher=MagicMock(), prefix="wp-content/plugins/my-plugin")
+            collector = HashCollector(path="/some/path/random1234", hasher=MagicMock(),
+                                      prefix="wp-content/plugins/my-plugin")
             collector.hasher.algo = "CONST"
             collector.hasher.hash.return_value = "12345"
 
@@ -47,9 +51,40 @@ class HashCollectorTest(TestCase):
 
             walk.assert_called_with("/some/path/random1234")
 
-            self.assertNotIn(Signature(path="wp-content/plugins/my-plugin/index.php", hash="12345", algo="CONST"), signatures)
-            self.assertNotIn(Signature(path="wp-content/plugins/my-plugin/js/index.php", hash="12345", algo="CONST"), signatures)
-            self.assertNotIn(Signature(path="wp-content/plugins/my-plugin/css/index.php", hash="12345", algo="CONST"), signatures)
+            self.assertNotIn(Signature(path="wp-content/plugins/my-plugin/index.php", hash="12345", algo="CONST"),
+                             signatures)
+            self.assertNotIn(Signature(path="wp-content/plugins/my-plugin/js/index.php", hash="12345", algo="CONST"),
+                             signatures)
+            self.assertNotIn(Signature(path="wp-content/plugins/my-plugin/css/index.php", hash="12345", algo="CONST"),
+                             signatures)
+
+    def test_flag_as_containing_version(self):
+        class FakeHasher:
+            algo = "CUST"
+
+            def hash(hasher, file_path, chunk_cb):
+                if file_path == "/some/path/random1234/readme.txt":
+                    chunk_cb(b"Readme for version 1.2.3 test")
+                    chunk_cb(b"weird chunk")
+                elif file_path == "/some/path/random1234/license.txt":
+                    chunk_cb(b"MIT...")
+                else:
+                    raise FileNotFoundError()
+                return "12345"
+
+        with patch('openwebvulndb.common.hash.walk') as walk:
+            walk.return_value = [
+                ("/some/path/random1234", [], ["readme.txt", "license.txt", "index.php"]),
+            ]
+            collector = HashCollector(path="/some/path/random1234", hasher=FakeHasher(),
+                                      prefix="wp-content/plugins/my-plugin", lookup_version="1.2.3")
+
+            signatures = list(collector.collect())
+
+            self.assertIn(Signature(path="wp-content/plugins/my-plugin/readme.txt",
+                                    hash="12345", algo="CUST", contains_version=True), signatures)
+            self.assertIn(Signature(path="wp-content/plugins/my-plugin/license.txt", hash="12345", algo="CUST"),
+                          signatures)
 
 
 class HasherTest(TestCase):
@@ -59,7 +94,8 @@ class HasherTest(TestCase):
 
         with patch('openwebvulndb.common.hash.open', m, create=True):
             hasher = Hasher('SHA256')
-            self.assertEqual('b94d27b9934d3e08a52e52d7da7dabfac484efe37a5380ee9088f7ace2efcde9', hasher.hash("/some/file.txt"))
+            self.assertEqual('b94d27b9934d3e08a52e52d7da7dabfac484efe37a5380ee9088f7ace2efcde9',
+                             hasher.hash("/some/file.txt"))
 
     def test_hash_md5(self):
         m = mock_open(read_data=b"hello world")
@@ -67,3 +103,45 @@ class HasherTest(TestCase):
         with patch('openwebvulndb.common.hash.open', m, create=True):
             hasher = Hasher('MD5')
             self.assertEqual('5eb63bbbe01eeed093cb22bb8f5acdc3', hasher.hash("/some/file.txt"))
+
+    def test_callback_applied_to_chunks(self):
+        m = mock_open(read_data=b"hello world")
+
+        with patch('openwebvulndb.common.hash.open', m, create=True):
+            check_chunk = MagicMock()
+
+            hasher = Hasher('SHA256')
+            hasher.hash("/some/file.txt", chunk_cb=check_chunk)
+
+            check_chunk.assert_called_with(b"hello world")
+
+
+class VersionCheckerTest(TestCase):
+
+    def test_checker_no_version(self):
+        checker = VersionChecker(None)
+        checker(b"hello world")
+        self.assertIsNone(checker.contains_version)
+
+    def test_checker_version_not_found(self):
+        checker = VersionChecker("1.2")
+        checker(b"hello world")
+        self.assertIsNone(checker.contains_version)
+
+    def test_checker_version_found(self):
+        checker = VersionChecker("1.2")
+        checker(b"hello world 1.2")
+        self.assertTrue(checker.contains_version)
+
+    def test_checker_version_found_multichunk(self):
+        checker = VersionChecker("1.2")
+        checker(b"hello world")
+        checker(b"hello world 1.2")
+        checker(b"hello world")
+        self.assertTrue(checker.contains_version)
+
+    def test_checker_version_found_but_resets(self):
+        checker = VersionChecker("1.2")
+        checker(b"hello world 1.2")
+        checker.reset()
+        self.assertIsNone(checker.contains_version)
