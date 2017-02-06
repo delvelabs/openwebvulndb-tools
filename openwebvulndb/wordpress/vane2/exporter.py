f58dd64b3696c890b6af1a2cd54205d82afcf364
nicolas <nicolas.aubry@delvelabs.ca>
2017-02-06 17:14:33 -0500

    Start integration of theme export in vane2_export.

--- openwebvulndb/wordpress/vane2/exporter.py
+++ openwebvulndb/wordpress/vane2/exporter.py
@@ -9,27 +9,50 @@ class Exporter:
     def __init__(self, storage):
         self.storage = storage
         self.plugins_list = FileListGroup(key="plugins", producer="Vane2Export")
+        self.themes_list = FileListGroup(key="themes", producer="Vane2Export")
 
-    def export_plugins(self, only_popular=False):
+    def export_plugins(self, only_popular=False, only_vulnerable=False):
         version_rebuild = VersionRebuild(self.storage)
-        for plugin_key in self._list_plugins_keys(only_popular):
+        for plugin_key in self._list_keys("plugins", only_popular, only_vulnerable):
             version_rebuild.update(plugin_key)
             plugin = version_rebuild.file_list
             self.plugins_list.file_lists.append(plugin)
 
+    def export_themes(self, only_popular=False, only_vulnerable=False):
+        version_rebuild = VersionRebuild(self.storage)
+        for theme_key in self._list_keys("themes", only_popular, only_vulnerable):
+            version_rebuild.update(theme_key)
+            theme_files = version_rebuild.file_list
+            self.themes_list.file_lists.append(theme_files)
+
     def dump_plugins(self):
-        input("press key to continue")
         return serialize(FileListGroupSchema(), self.plugins_list)
 
-    def _list_plugins_keys(self, only_popular=False):
+    def _list_keys(self, key, only_popular=False, only_vulnerable=False):
         if only_popular:
-            yield from self._list_popular_plugins_keys()
+            yield from self._list_popular(key)
+        elif only_vulnerable:
+            yield from self._list_vulnerable(key)
         else:
-            yield from self._list_all_plugins_keys()
+            yield from self._list_all_keys(key)
+
+    def _list_vulnerable(self, key):
+        for _key, files in self._walk(key):
+            if self._contains_vuln_file(files):
+                yield _key
+
+    def _list_popular(self, key):
+        for _key, files in self._walk(key):
+            if self._is_popular(_key, files):
+                yield _key
+
+    def _list_all_keys(self, key):
+        for _key, files in self._walk(key):
+            yield _key
 
     def _list_popular_plugins_keys(self):
         for key, path, dirnames, files in self.storage.walk("plugins"):
-            if "versions.json" in files and self._is_plugin_popular(key, files):
+            if "versions.json" in files and self._is_popular(key, files):
                 yield key
 
     def _list_all_plugins_keys(self):
@@ -37,8 +60,19 @@ class Exporter:
             if "versions.json" in files:
                 yield key
 
-    def _is_plugin_popular(self, key, files):
+    def _is_popular(self, key, files):
         if "META.json" in files:
             meta = self.storage.read_meta(key)
             return meta.is_popular
         return False
+
+    def _contains_vuln_file(self, files):
+        for file in files:
+            if file.startswith("vuln-") and file.endswith(".json"):
+                return True
+        return False
+
+    def _walk(self, key):
+        for key, path, dirnames, files in self.storage.walk(key):
+            if "versions.json" in files:
+                yield key, files
--- tests/wordpress_test/vane2_test/exporter_test.py
+++ tests/wordpress_test/vane2_test/exporter_test.py
@@ -12,11 +12,11 @@ class ExporterTest(TestCase):
         self.exporter = Exporter(self.storage)
 
     def test_export_plugins_regroup_plugins_in_one_file(self):
-        plugin0_versions_list = VersionList(key="plugin0", producer="unittest")
-        plugin1_versions_list = VersionList(key="plugin1", producer="unittest")
-        self.exporter._list_plugins_keys = MagicMock()
-        self.exporter._list_plugins_keys.return_value = ["plugin0", "plugin1"]
-        self.storage.plugins_versions_list = [plugin0_versions_list, plugin1_versions_list]
+        plugin0_version_list = VersionList(key="plugin0", producer="unittest")
+        plugin1_version_list = VersionList(key="plugin1", producer="unittest")
+        self.exporter._list_keys = MagicMock()
+        self.exporter._list_keys.return_value = ["plugin0", "plugin1"]
+        self.storage.version_list = [plugin0_version_list, plugin1_version_list]
 
         self.exporter.export_plugins()
 
@@ -31,10 +31,10 @@ class ExporterTest(TestCase):
         versions = [VersionDefinition(version="1.0", signatures=[Signature(path="file.html", hash="a1b2c3")]),
                     VersionDefinition(version="2.0", signatures=[Signature(path="file.html", hash="d4e5f6"),
                                                                  Signature(path="style.css", hash="12345")])]
-        plugin_versions_list = VersionList(key="my-plugin", producer="unittest", versions=versions)
-        self.exporter._list_plugins_keys = MagicMock()
-        self.exporter._list_plugins_keys.return_value = ["my-plugin"]
-        self.storage.plugins_versions_list = [plugin_versions_list]
+        plugin_version_list = VersionList(key="my-plugin", producer="unittest", versions=versions)
+        self.exporter._list_keys = MagicMock()
+        self.exporter._list_keys.return_value = ["my-plugin"]
+        self.storage.version_list = [plugin_version_list]
 
         self.exporter.export_plugins()
 
@@ -54,9 +54,53 @@ class ExporterTest(TestCase):
         self.assertEqual(path_version2_signature.versions, ["2.0"])
         self.assertEqual(style_signature.versions, ["2.0"])
 
+    def test_export_themes_regroup_themes_in_one_file(self):
+        theme0_version_list = VersionList(key="theme0", producer="unittest")
+        theme1_version_list = VersionList(key="theme1", producer="unittest")
+        self.exporter._list_keys = MagicMock()
+        self.exporter._list_keys.return_value = ["theme0", "theme1"]
+        self.storage.version_list = [theme0_version_list, theme1_version_list]
+
+        self.exporter.export_themes()
+
+        themes = self.exporter.themes_list
+        self.assertEqual(themes.key, "themes")
+        self.assertEqual(themes.producer, "Vane2Export")
+        self.assert_object_with_attribute_value_in_container("key", "theme0", themes.file_lists)
+        self.assert_object_with_attribute_value_in_container("producer", "Vane2Export", themes.file_lists)
+        self.assert_object_with_attribute_value_in_container("key", "theme1", themes.file_lists)
+
+    def test_export_themes_create_file_list_from_version_list_for_theme(self):
+        versions = [VersionDefinition(version="1.0", signatures=[Signature(path="file.html", hash="a1b2c3")]),
+                    VersionDefinition(version="2.0", signatures=[Signature(path="file.html", hash="d4e5f6"),
+                                                                 Signature(path="style.css", hash="12345")])]
+        theme_version_list = VersionList(key="my-theme", producer="unittest", versions=versions)
+        self.exporter._list_keys = MagicMock()
+        self.exporter._list_keys.return_value = ["my-theme"]
+        self.storage.version_list = [theme_version_list]
+
+        self.exporter.export_themes()
+
+        themes = self.exporter.themes_list
+        theme = themes.file_lists[0]
+        self.assertEqual(theme.key, "my-theme")
+        self.assert_object_with_attribute_value_in_container("path", "file.html", theme.files)
+        self.assert_object_with_attribute_value_in_container("path", "style.css", theme.files)
+        path_file = self.get_object_with_attribute_value_in_container("path", "file.html", theme.files)
+        style_file = self.get_object_with_attribute_value_in_container("path", "style.css", theme.files)
+        path_version1_signature = self.get_object_with_attribute_value_in_container("hash", "a1b2c3",
+                                                                                    path_file.signatures)
+        path_version2_signature = self.get_object_with_attribute_value_in_container("hash", "d4e5f6",
+                                                                                    path_file.signatures)
+        style_signature = style_file.signatures[0]
+        self.assertEqual(path_version1_signature.versions, ["1.0"])
+        self.assertEqual(path_version2_signature.versions, ["2.0"])
+        self.assertEqual(style_signature.versions, ["2.0"])
+
     def test_list_plugins_keys(self):
+        self.skipTest("Change test")
         files = ["versions.json"]
-        self.storage.walk.return_value = [("plugins/plugin0", "dirpath", "dirnames", files),
+        self.storage.content = [("plugins/plugin0", "dirpath", "dirnames", files),
                                           ("plugins/plugin1", "dirpath", "dirnames", files)]
 
         plugins_keys = list(self.exporter._list_plugins_keys())
@@ -65,11 +109,12 @@ class ExporterTest(TestCase):
         self.assertIn("plugins/plugin1", plugins_keys)
 
     def test_list_plugins_keys_list_popular_plugins(self):
+        self.skipTest("Change test")
         self.storage.meta_list = [Meta(key='plugins/plugin0', is_popular=True),
                                   Meta(key='plugins/plugin1', is_popular=False)]
         files = ["META.json", "versions.json"]
-        self.storage.walk.return_value = [("plugins/plugin0", "dirpath", "dirnames", files),
-                                     ("plugins/plugin1", "dirpath", "dirnames", files)]
+        self.storage.content = [("plugins/plugin0", "dirpath", "dirnames", files),
+                                ("plugins/plugin1", "dirpath", "dirnames", files)]
 
         plugins_keys = list(self.exporter._list_plugins_keys(only_popular=True))
 
@@ -77,15 +122,74 @@ class ExporterTest(TestCase):
         self.assertNotIn("plugins/plugin1", plugins_keys)
 
     def test_list_plugins_keys_skip_plugins_without_version_file(self):
-        self.storage.meta_list = [Meta(key='plugins/plugin0'),
-                                  Meta(key='plugins/plugin1')]
-        self.storage.walk.return_value = [("plugins/plugin0", "dirpath", "dirnames", ["META.json", "versions.json"]),
+        self.skipTest("Change test")
+        self.storage.content = [("plugins/plugin0", "dirpath", "dirnames", ["META.json", "versions.json"]),
                                           ("plugins/plugin1", "dirpath", "dirnames", ["META.json"])]
 
         plugins_keys = list(self.exporter._list_plugins_keys())
 
         self.assertIn("plugins/plugin0", plugins_keys)
         self.assertNotIn("plugins/plugin1", plugins_keys)
+
+    def test_walk_yield_keys_and_files_in_storage_if_versions_file_is_present(self):
+        self.storage.content = [("plugins/plugin0", "dirpath", "dirnames", ["META.json", "versions.json"]),
+                                          ("plugins/plugin1", "dirpath", "dirnames", ["META.json"]),
+                                          ("themes/theme0", "dirpath", "dirnames", ["META.json", "versions.json"]),
+                                          ("themes/theme1", "dirpath", "dirnames", ["META.json"])]
+
+        for key, files in self.exporter._walk("plugins"):
+            self.assertEqual(key, "plugins/plugin0")
+            self.assertEqual(files, ["META.json", "versions.json"])
+
+        for key, files in self.exporter._walk("themes"):
+            self.assertEqual(key, "themes/theme0")
+            self.assertEqual(files, ["META.json", "versions.json"])
+
+    def test_list_vulnerable_list_keys_of_plugins_or_themes_with_vuln(self):
+        files = ["META.json", "versions.json"]
+        vuln_files0 = ["META.json", "versions.json", "vuln-cvereader.json"]
+        vuln_files1 = ["META.json", "versions.json", "vuln-securityfocus.json"]
+        self.storage.content = [("plugins/plugin0", "dirpath", "dirnames", files),
+                                ("plugins/plugin1", "dirpath", "dirnames", vuln_files0),
+                                ("themes/theme0", "dirpath", "dirnames", files),
+                                ("themes/theme1", "dirpath", "dirnames", vuln_files1)]
+
+        plugin_key = list(self.exporter._list_vulnerable("plugins"))
+        theme_key = list(self.exporter._list_vulnerable("themes"))
+        self.assertEqual(plugin_key, ["plugins/plugin1"])
+        self.assertEqual(theme_key, ["themes/theme1"])
+
+    def test_list_popular_list_keys_of_popular_plugins_or_themes(self):
+        files = ["META.json", "versions.json"]
+        self.storage.meta_list = [Meta(key='plugins/plugin0', is_popular=True),
+                                  Meta(key='plugins/plugin1', is_popular=False),
+                                  Meta(key='themes/theme0', is_popular=True),
+                                  Meta(key='themes/theme1', is_popular=False)]
+        self.storage.content = [("plugins/plugin0", "dirpath", "dirnames", files),
+                                ("plugins/plugin1", "dirpath", "dirnames", files),
+                                ("themes/theme0", "dirpath", "dirnames", files),
+                                ("themes/theme1", "dirpath", "dirnames", files)]
+
+        plugin_key = list(self.exporter._list_popular("plugins"))
+        theme_key = list(self.exporter._list_popular("themes"))
+        self.assertEqual(plugin_key, ["plugins/plugin0"])
+        self.assertEqual(theme_key, ["themes/theme0"])
+
+    def test_list_all_keys_list_all_keys_within_key(self):
+        files = ["META.json", "versions.json"]
+        self.storage.content = [("plugins/plugin0", "dirpath", "dirnames", files),
+                                ("plugins/plugin1", "dirpath", "dirnames", files),
+                                ("themes/theme0", "dirpath", "dirnames", files),
+                                ("themes/theme1", "dirpath", "dirnames", files)]
+
+        plugin_keys = list(self.exporter._list_all_keys("plugins"))
+        theme_keys = list(self.exporter._list_all_keys("themes"))
+        self.assertIn("plugins/plugin0", plugin_keys)
+        self.assertIn("plugins/plugin1", plugin_keys)
+        self.assertEqual(len(plugin_keys), 2)
+        self.assertIn("themes/theme0", theme_keys)
+        self.assertIn("themes/theme1", theme_keys)
+        self.assertEqual(len(theme_keys), 2)
 
     def assert_object_with_attribute_value_in_container(self, attribute, value, container):
         if self.get_object_with_attribute_value_in_container(attribute, value, container) is None:
@@ -102,13 +206,18 @@ class ExporterTest(TestCase):
 
 class FakeStorage:
 
-    def __init__(self, plugins_versions_list, meta_list=None):
-        self.plugins_versions_list = plugins_versions_list or []
+    def __init__(self, version_list, meta_list=None):
+        self.version_list = version_list or []
         self.meta_list = meta_list or []
-        self.walk = MagicMock()
+        self.content = []
+
+    def walk(self, key):
+        for _key, path, dirnames, files in self.content:
+            if key in _key:
+                yield _key, path, dirnames, files
 
     def read_versions(self, key):
-        for version_list in self.plugins_versions_list:
+        for version_list in self.version_list:
             if version_list.key == key:
                 return version_list
         return None
