846feb40a52f6eb6e9849e151ce52010a98e342f
Nicolas <nicolas.aubry@delvelabs.ca>
2017-07-24 17:05:56 -0400

    Add function to generate json of cvss based on type.

--- openwebvulndb/common/__main__.py
+++ openwebvulndb/common/__main__.py
@@ -18,6 +18,9 @@
 from argparse import ArgumentParser
 
 from openwebvulndb import app
+from statistics import mean
+import json
+import os
 
 
 def find_identity_files(storage, input_key):
@@ -47,8 +50,37 @@ def find_unclosed_vulnerabilities(storage, input_filter):
                 if not v.affected_versions or any(r.fixed_in is None for r in v.affected_versions):
                     print("{l.key: <60} {l.producer: <15} {v.id: <20} {v.title}".format(l=vlist, v=v))
 
+
+def find_cvss_based_on_common_vulnerability_type(storage):
+    types = {}
+    for meta in storage.list_meta():
+        for vuln_list in storage.list_vulnerabilities(meta.key):
+            for vuln in vuln_list.vulnerabilities:
+                if vuln.reported_type is not None and vuln.reported_type.lower() != "unknown":
+                    if not vuln.reported_type in types:
+                        types[vuln.reported_type] = {"count": 0, "cvss": []}
+                    types[vuln.reported_type]["count"] += 1
+                    if vuln.cvss is not None:
+                        types[vuln.reported_type]["cvss"].append(vuln.cvss)
+
+    #sorted_types = sorted(types.keys(), key=lambda key: types[key]["count"], reverse=True)
+    cvss_from_type = {}
+    sorted_types = types.keys()
+    for type in sorted_types:
+        data = types[type]
+        len_cvss = len(data["cvss"])
+        if len_cvss > 0 and len_cvss != data["count"]:  # Don't need the mean f all vuln already have a cvss.
+            if len_cvss > 5:  # Else calculated cvss is not significant
+                cvss_mean = mean(data["cvss"])
+                cvss_from_type[type] = cvss_mean
+                print("{0}: \t{1} times in database, \tmean cvss is {2} (from {3} cvss)".format(type, data["count"], cvss_mean, len_cvss))
+    with open(os.path.join(os.path.dirname(__file__), "cvss_from_reported_type.json"), "wt") as fp:
+        json.dump(cvss_from_type, fp, indent=4)
+
+
 operations = dict(find_identity_files=find_identity_files,
-                  find_unclosed_vulnerabilities=find_unclosed_vulnerabilities)
+                  find_unclosed_vulnerabilities=find_unclosed_vulnerabilities,
+                  find_cvss_based_on_common_vulnerability_type=find_cvss_based_on_common_vulnerability_type)
 
 
 parser = ArgumentParser(description="OpenWebVulnDb Data Collector")
